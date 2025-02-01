305a207f91ee1203960f842a4d6bc924c91cf736
Sanchit <sanchiue@gmail.com>
2025-02-01 18:57:20 +0530

    Create highestAiltitude.java

---
+++ highestAiltitude.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int n=gain.length;
+        int temp=0;
+        int max=0;
+        
+        for(int i=0;i<n;i++){
+          temp+=gain[i];
+           max=Math.max(max,temp);
+           
+        }
+        return max;
+    }
+}
