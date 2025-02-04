1f301a1a9e9c217733bc82519d0f89b4190edbc8
Sanchit <sanchiue@gmail.com>
2025-02-04 22:12:32 +0530

    Create max_no_ofvowel.java

---
+++ java/max_no_ofvowel.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxVowels(String s, int k) {
+        String vowels="aeiouAEIOU";
+        int count=0;
+        int max=0;
+
+        for(int i=0;i<k;i++){
+            if(vowels.indexOf(s.charAt(i))!=-1){
+                count++;
+            }
+        }
+        max=count;
+        for(int i=k;i<s.length();i++){
+            if(vowels.indexOf(s.charAt(i))!=-1){
+                count++;
+            }
+            if (vowels.indexOf(s.charAt(i - k)) != -1) { // Remove old character
+                count--;
+            }
+            max=Math.max(max,count);
+        }
+        return max;
+    }
+}
